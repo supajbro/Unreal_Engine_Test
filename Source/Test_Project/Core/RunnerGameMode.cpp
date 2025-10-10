492bed2d230769f744a57850fffc91e02dbe4cd5
supajbro <supajbro@gmail.com>
2025-10-10 20:32:27 +1000

    Adding all base classes

---
+++ Source/Test_Project/Core/RunnerGameMode.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/RunnerGameMode.h"
+
+RunnerGameMode::RunnerGameMode()
+{
+}
+
+RunnerGameMode::~RunnerGameMode()
+{
+}
---
+++ Source/Test_Project/Core/RunnerPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/RunnerPlayerController.h"
+
+RunnerPlayerController::RunnerPlayerController()
+{
+}
+
+RunnerPlayerController::~RunnerPlayerController()
+{
+}
